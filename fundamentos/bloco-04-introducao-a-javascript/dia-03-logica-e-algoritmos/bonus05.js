dc80d36a5e0da380c2bb6d3936a666edecf978c6
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-08-27 20:46:31 -0300

    todos os bonus OK (easteregg piramide de gise)

--- fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-e-algoritmos/bonus05.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-03-logica-e-algoritmos/bonus05.js
@@ -1,26 +1,25 @@
-// imprima uma pirâmide de base n (impar) [vazia]
-let n = 7;
+// imprima uma pirâmide de base b (impar) [vazia]
+let b = 1111;                       // pirâmide de Gizé
 //Altura = n /2 arredondado pra cima
-let h = Math.ceil(n/2);         
-// //Inicia a array com altura-1 espaços
-// let linha = Array(h).join(" ");
+let h = Math.ceil(b/2);             // altura h
 
-// for (h; h>0; h--) {
-//     linha = linha.concat("*");  // +1 * no final
-//     console.log(linha);         // imprime a linha
-//     linha = linha.concat("*");  // +1 * no final
-//     linha = linha.substr(1);    // remove array[0]
-//     linha[h] = "e";
-// }
-// // Muita tentativa e erro!
-
-let linha = [];
-for(let i = 0; i < n; i +=1 ){
-    linha.push("*");
-    let s = "";
-    for (let l = 0; l < linha.length; l+=1)
-        s = s.concat(linha[l]);
-
-    console.log(s);
+let p = "";                         // camadas da pirâmide
+for (let i = 0; i < h-1; i+=1) {
+    p = p.concat(" ");
 }
+p = p.concat("* ");
 
+// Imprime o corpo
+for (let i = 0; i < h-1; i+=1) {
+    // console.log(p,i);    //debug
+    console.log(p);
+    p = p.substr(0, p.length-1);    // remove o ultimo*
+    if(i) p = p.concat(" ");        // na segunda linha não põe o espaço extra
+    p = p.concat(" *");             // add * no fim
+    p = p.substr(1);                // remove p[0]   
+}
+//Imprime a base
+p = "";
+for (let i = 0; i < b; i+=1) p = p.concat("*");
+// console.log(p,h-1);      //debug
+console.log(p);
