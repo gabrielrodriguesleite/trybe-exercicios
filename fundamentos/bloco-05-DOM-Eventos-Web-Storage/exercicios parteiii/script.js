270e5d63f81bc9f9d7fbf0779539c3035e6c9105
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-02 19:54:28 -0300

    exercicio parte 3

---
+++ fundamentos/bloco-05-DOM-Eventos-Web-Storage/exercicios parteiii/script.js
@@ -0,0 +1,30 @@
+// Cabeçalho
+cab = document.querySelector("#header-container");
+cab.style.backgroundColor = '#0c6';
+
+// 
+
+
+
+
+for (let i of document.querySelectorAll(".emergency-tasks h3"))
+    i.style.backgroundColor = '#d8d';
+
+
+// for (let i of document.querySelectorAll(".emergency-tasks"))
+// i.style.backgroundColor = '#fbb';
+
+UT_BG = document.querySelector(".emergency-tasks");
+UT_BG.style.backgroundColor = '#fbb';
+// UT_BG.style.display = 'inline-block';
+
+for (let i of document.querySelectorAll(".no-emergency-tasks h3"))
+    i.style.backgroundColor = '#111';
+    
+// for (let i of document.querySelectorAll(".no-emergency-tasks"))
+//     i.style.backgroundColor = '#fc4';
+
+NU_BG = document.querySelector(".no-emergency-tasks");
+NU_BG.style.backgroundColor = '#fc4';
+
+// NU_BG.style.display = 'inline-block';
