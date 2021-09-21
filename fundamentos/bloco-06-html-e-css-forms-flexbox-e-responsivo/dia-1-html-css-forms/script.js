94e73b6662cd5423833aaedc3052defa48b83d63
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-20 23:09:58 -0300

    modals inicio - editando o modal de erro

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -12,17 +12,40 @@ function carregaEstados() {
   }
 }
 
+// função erro!
+function erro(n) {
+  console.log('erro',n);
+  if (n === 1) {
+    console.log('data informada está incorreta')
+
+    document.querySelector('#input-data-inicio').scrollIntoView();
+    document.querySelector('#input-data-inicio').style.border = '2px red solid';
+    document.querySelector('#titulo-erro').innerText = 'Erro na Data';
+    document.querySelector('#texto-erro').innerText = 'A data informada é inválida.';
+    document.querySelector('#container-erro').classList.remove('oculto');
+    document.querySelector('#blackout-modal').classList.remove('oculto');
+  }
+}
+
 // data inicio [regex = expressão regular ]
 const rx = /([0-9]{2})\/([0-9]{2})\/([0-9]{2})/
 function testDataCargo() {
   const data = rx.exec(document.querySelector('#input-data-inicio').value);
-  console.log(`${data[1]}\/${data[2]}\/${data[3]}`);
+  if (data != null) console.log(`${data[1]}\/${data[2]}\/${data[3]}`);
+  erro(1);
 }
 
 // enviar → criar div consolidando os dados inseridos
 // mostrar erros em uma mensagem de erro na div
 
 function configurar() {
+
+  // Oculta diálogos modais - funcionou melhor inline (segura essa linter!)
+  // Acabei usando a classe oculto. Talvez seja preciso setar o display:block|flex
+  // document.querySelector('#container-erro').style.display = 'none';
+  // document.querySelector('#container-resultado').style.display = 'none';
+  // document.querySelector('#blackout-modal').style.display = 'none';
+  // document.querySelector('#blackout-modal').style.opacity = '0.8';
   carregaEstados();
   document.querySelector('#botao-enviar').addEventListener('click', testDataCargo);
 }
