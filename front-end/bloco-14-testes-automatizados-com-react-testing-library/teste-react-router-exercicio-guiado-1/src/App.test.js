d4ae96621f6b2163159791f4b82231cf5e58cac9
[Gabriel Rodrigues Leite] <k.cogabriel@gmail.com>
2021-11-29 18:57:47 -0300

    [new] exercicio guiado ok

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/teste-react-router-exercicio-guiado-1/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/teste-react-router-exercicio-guiado-1/src/App.test.js
@@ -1,24 +1,34 @@
-import { React } from 'react';
+// import { React } from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from './App';
 import userEvent from '@testing-library/user-event';
 
-// it('deve renderizar o componente App', () => {
-//   renderWithRouter(<App />);
-//   const homeTitle = screen.getByRole('heading', {
-//     name: 'Você está na página Início',
-//   });
-//   expect(homeTitle).toBeInTheDocument();
-// });
+describe('teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    renderWithRouter(<App />);
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
 
-it('deve renderizar o componente Sobre', () => {
-  const { history } = renderWithRouter(<App />);
-  const aboutLink = screen.getByRole('link', { name: 'Sobre' });
-  expect(aboutLink).toBeInTheDocument();
-  userEvent.click(aboutLink);
-  
-  // console.log(history);
-  const { pathname } = history.location;
-  expect(pathname).toBe('/about');
+  it('deve renderizar o componente Sobre', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: 'Sobre' });
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+    
+    // console.log(history);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('deve testar o caminho não existente e a renderização do Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const notFoundTitle = screen.getByRole('heading',
+      { name: 'Página não encontrada' });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
 });
